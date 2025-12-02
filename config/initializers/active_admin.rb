e90b9a51521073dc272666e0ffb5f39dd8cec417
Maxim Pervushin <pervushin@telega.in>
2025-12-02 15:02:16 +0500

    fix

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -3,7 +3,10 @@ ActiveAdmin.setup do |config|
   #
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
-  #
+
+  config.namespace :admin do |admin|
+    admin.route_options = { path: ENV['ADMIN_URL'] }
+  end
   config.site_title = "Russvisa"
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
